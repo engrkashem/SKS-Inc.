6bd253a9abe40a9ca2fea935155e44d5096a5af1
engrkashem <kashemaust@gmail.com>
2022-05-26 13:02:48 +0600

    not found page added

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import bgNotFound from '../../images/bgNotFound.jpg';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div className='bg-cover h-screen' style={{ backgroundImage: `url(${bgNotFound})` }}>
-            <h2>404, Not found</h2>
+        <div className='bg-cover bg-center min-h-screen' style={{ backgroundImage: `url(${bgNotFound})` }}>
+            <button onClick={() => navigate('/home')} className='custom-button btn-lg text-white relative top-[470px] left-[630px] font-bold border-4 border-emerald-400'><Link to={'/home'}> Go to HOME</Link></button>
         </div>
     );
 };
