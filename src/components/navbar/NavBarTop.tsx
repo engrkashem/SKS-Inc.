9dab649101169bd2459ec14399f41ce02699e9a1
engrkashem <kashemaust@gmail.com>
2024-06-05 22:19:39 +0600

    logo and login route added

--- src/components/navbar/NavBarTop.tsx
+++ src/components/navbar/NavBarTop.tsx
@@ -1,20 +1,34 @@
 import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
 import { Button, Layout } from 'antd';
+import logo from '../../assets/react.svg';
 const { Header, Sider, Content } = Layout;
 
 const NavBarTop = ({ collapsed, setCollapsed }) => {
   return (
-    <Header style={{ padding: 0, background: '#ffffff' }}>
-      <Button
-        type="text"
-        icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-        onClick={() => setCollapsed(!collapsed)}
-        style={{
-          fontSize: '16px',
-          width: 64,
-          height: 64,
-        }}
-      />
+    <Header
+      style={{
+        padding: 0,
+        background: '#ffffff',
+        display: 'flex',
+        justifyContent: 'space-between',
+        padding: '10px',
+      }}
+    >
+      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+        <Button
+          type="text"
+          icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+          onClick={() => setCollapsed(!collapsed)}
+          style={{
+            fontSize: '16px',
+            width: 64,
+            height: 64,
+          }}
+        />
+        {/* <Button>Hello</Button> */}
+        <img style={{ height: '20px' }} src={logo} alt="" srcSet="" />
+      </div>
+      <Button>Login</Button>
     </Header>
   );
 };
